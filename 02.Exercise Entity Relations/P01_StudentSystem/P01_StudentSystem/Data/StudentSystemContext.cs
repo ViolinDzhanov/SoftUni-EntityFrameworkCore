1634a139c72efb5912c508e88bd71529ddd3b746
ViolinDzhanov <122802081+ViolinDzhanov@users.noreply.github.com>
2024-06-30 10:24:07 +0100

    Add files via upload

---
+++ 02.Exercise Entity Relations/P01_StudentSystem/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        private const string ConnectionString =
+            "Server=.;Database=StudentSystem ;Integrated Security=True";
+
+        public StudentSystemContext(DbContextOptions dbContextOptions) 
+            : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<StudentCourse> StudentsCourses { get; set; }
+        public DbSet<Homework> Homeworks { get; set; }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(ConnectionString);
+        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>()
+                .HasKey(sc => new
+                {
+                    sc.StudentId,
+                    sc.CourseId,
+                });
+        }
+    }
+}
