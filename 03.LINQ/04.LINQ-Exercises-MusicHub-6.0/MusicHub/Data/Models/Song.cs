4a14a090aab10f682e70e22dccaccf1ec3372573
ViolinDzhanov <122802081+ViolinDzhanov@users.noreply.github.com>
2024-06-30 20:38:25 +0100

    Add files via upload

---
+++ 03.LINQ/04.LINQ-Exercises-MusicHub-6.0/MusicHub/Data/Models/Song.cs
@@ -0,0 +1,45 @@
+﻿using MusicHub.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MusicHub.Data.Models
+{
+    public class Song
+    {
+
+        public Song()
+        {
+            SongPerformers = new HashSet<SongPerformer>();
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [ForeignKey(nameof(Album))]
+        public int? AlbumId { get; set; }
+        public virtual Album Album { get; set; }
+
+        [ForeignKey(nameof(Writer))]
+        [Required]
+        public int? WriterId { get; set; }
+        public virtual Writer Writer { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public ICollection<SongPerformer> SongPerformers { get; set; }
+    }
+}
