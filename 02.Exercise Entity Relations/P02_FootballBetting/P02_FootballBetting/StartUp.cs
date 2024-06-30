2149c76cfb4fbde47a99225fb9b7315718541eb5
ViolinDzhanov <122802081+ViolinDzhanov@users.noreply.github.com>
2024-06-30 20:41:52 +0100

    Add files via upload

---
+++ 02.Exercise Entity Relations/P02_FootballBetting/P02_FootballBetting/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using P02_FootballBetting.Data;
+namespace P02_FootballBetting
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
