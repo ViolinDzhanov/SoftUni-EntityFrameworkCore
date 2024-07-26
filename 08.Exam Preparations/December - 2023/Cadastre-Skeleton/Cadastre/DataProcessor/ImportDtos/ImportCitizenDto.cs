99f8cebf8581813e92bccb96860dc38932e2f038
ViolinDzhanov <122802081+ViolinDzhanov@users.noreply.github.com>
2024-07-26 20:45:15 +0100

    Add files via upload

---
+++ 08.Exam Preparations/December - 2023/Cadastre-Skeleton/Cadastre/DataProcessor/ImportDtos/ImportCitizenDto.cs
@@ -0,0 +1,36 @@
+﻿namespace Cadastre.DataProcessor.ImportDtos
+{
+    using Cadastre.Data.Enumerations;
+    using Cadastre.Data.Models;
+    using Newtonsoft.Json;
+    using System.ComponentModel.DataAnnotations;
+
+    [JsonObject(nameof(Citizen))]
+    public class ImportCitizenDto
+    {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(30)]
+        [JsonProperty(nameof(FirstName))]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(30)]
+        [JsonProperty(nameof(LastName))]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [JsonProperty(nameof(BirthDate))]
+        public string BirthDate { get; set; } = null!;
+
+        [Required]
+        [EnumDataType(typeof(MaritalStatus))]
+        [JsonProperty(nameof(MaritalStatus))]
+        public string MaritalStatus { get; set; } = null!;
+
+        [Required]
+        [JsonProperty(nameof(Properties))]
+        public int[] Properties { get; set; } = null!;
+    }
+}
